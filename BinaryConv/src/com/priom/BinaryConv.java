7e705a6f3b9bb95b813cff152edae06f08809048
ki11er <deathrazer007@yahoo.com>
2015-11-14 04:11:05 -0400

    Added seperate functions for each conversion

--- BinaryConv/src/com/priom/BinaryConv.java
+++ BinaryConv/src/com/priom/BinaryConv.java
@@ -6,9 +6,6 @@ public class BinaryConv {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
 
-        int index;
-        int result;
-
         System.out.print ("Type 1 to convert binary to decimal\nType 2 to convert decimal to binary: ");
         int choice = k.nextInt ();
 
@@ -16,46 +13,50 @@ public class BinaryConv {
             // Taking an input from user
             System.out.print ("Enter a binary number: ");
             String n = k.next ();
-
-            result = 0;
-
-            // Calculating the binary to decimal
-
-            if (n.length () > 31)
-                System.out.println ("Invalid input: " + n + " : length greater than 31 characters.");
-
-            else {
-                for (index = 0; index <= n.length () - 1; index++) {
-                    if (n.charAt (index) == '1') {
-                        result = (result * 2) + 1;
-                    } else if (n.charAt (index) == '0') {
-                        result = (result * 2);
-                    } else {
-                        System.out.println ("Invalid character: " + n + " : not a binary digit (0 or 1).");
-                        return;
-                    }
-                }
-                System.out.println (n + " (base 2) = " + result + " (base 10)");
-            }
+            System.out.println (n + " (base 2) = " + bin2dec (n) + " (base 10)");
         }
         else {
             System.out.print ("Enter a decimal number: ");
             int dec = k.nextInt ();
-            int d = dec;
-            String bin = "";
 
-            if (d == 0) {
-                bin = "0";
-            }
+            System.out.printf ("%d (base 10) = %s (base 2)\n", dec, dec2bin (dec));
+        }
+    }
 
-            else {
-                while (d > 0) {
-                    int rem = d % 2;
-                    bin = rem + bin;
-                    d = d / 2;
+    public static int bin2dec(String b) {
+        int result = 0;
+
+        if (b.length () > 31) {
+            System.out.println ("Invalid input: " + b + " : length greater than 31 characters.");
+        }
+        else {
+            for (int index = 0; index <= b.length () - 1; index++) {
+                if (b.charAt (index) == '1') {
+                    result = (result * 2) + 1;
+                } else if (b.charAt (index) == '0') {
+                    result = (result * 2);
+                } else {
+                    result = -1;
                 }
             }
-            System.out.printf ("%d (base 10) = %s (base 2)\n", dec, bin);
         }
+        return result;
+
+    }
+
+    public static String dec2bin(int d) {
+        String bin = "";
+        if (d == 0) {
+            bin = "0";
+        }
+
+        else {
+            while (d > 0) {
+                int rem = d % 2;
+                bin = rem + bin;
+                d = d / 2;
+            }
+        }
+        return bin;
     }
 }
